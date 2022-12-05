d5306479ebb16908828da76de7bddf76e810344e
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2022-12-05 10:32:30 -0300

    Creando Proyecto y Solucion

---
+++ leerData/Libro.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leerData
+{
+    public class Libro
+    {
+        public int LibroID { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        
+    }
+}
---
+++ leerData/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace leerData
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
